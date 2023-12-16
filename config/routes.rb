f331b59315e01478890d30e7878ea91a40892b52
jonathan Dipita <jonathandipita@gmail.com>
2023-12-16 15:33:02 +0400

    create done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
   get 'tasks', to: 'tasks#index'
   get 'tasks/:id', to: 'tasks#show', as: :show
 end
